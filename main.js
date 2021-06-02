c769f29f850dcaf267a03889c4c9a1d0dabe3e09
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-06-02 08:11:01 +0100

    added menu to dict

--- main.js
+++ main.js
@@ -18,8 +18,20 @@ const dict = {
         ga: `Is é seo wiki fa choinne RLI`
     },
     langChangeText: {
-        en: `This page is available in <button type="button" onclick="langChange()" class="text-button"><strong>English</strong></button>.`,
-        ga: `Tá an leitheanach seo ar fáil i <button type="button" onclick="langChange()" class="text-button"><strong>nGaeilge</strong></button>.`
+        en: `Tá an leitheanach seo ar fáil i <button type="button" onclick="langChange()" class="text-button"><strong>nGaeilge</strong></button>.`,
+        ga: `This page is available in <button type="button" onclick="langChange()" class="text-button"><strong>English</strong></button>.`
+    },
+    headerBanner: {
+        en: `<li><a href="index.html">Home</a></li>
+        <li><a href="Timeline/timeline.html">Timeline</a></li>
+        <li><a href="Running-Jokes/running-jokes.html">Running Jokes</a></li>
+        <li><a href="Copypastas/copypastas.html">Copypastas</a></li>
+        <li><a href="Personalities/personalities.html">Personalities</a></li>`,
+        ga: `<li><a href="index.html">Baile</a></li>
+        <li><a href="Timeline/timeline.html">Amlíne</a></li>
+        <li><a href="Running-Jokes/running-jokes.html">Ábhair Grinn</a></li>
+        <li><a href="Copypastas/copypastas.html">Turscar</a></li>
+        <li><a href="Personalities/personalities.html">Pearsantachtaí</a></li>`
     }
 }
 
@@ -39,10 +51,12 @@ if (localStorage.getItem("lang") == 1) {
     document.getElementById("homepageTitle").innerHTML = dict.mainTitle.en;
     document.getElementById("innerPageContent").innerHTML = dict.mainContent.en;
     document.getElementById("langChangeText").innerHTML = dict.langChangeText.en;
+    document.getElementById("headerBanner").innerHTML = dict.headerBanner.en;
 } else if (localStorage.getItem("lang") == 2) {
     document.getElementById("homepageTitle").innerHTML = dict.mainTitle.ga;
     document.getElementById("innerPageContent").innerHTML = dict.mainContent.ga;
     document.getElementById("langChangeText").innerHTML = dict.langChangeText.ga;
+    document.getElementById("headerBanner").innerHTML = dict.headerBanner.ga;
 } else {
     console.log("Error: language not loaded correctly");
 }
