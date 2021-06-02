ce1e5b424c4377d36822560d7f90db31e457070f
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-06-02 07:54:54 +0100

    FINALLY GOT THE FUCKIN THING TO WORK

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ if (!firstTime) {
 }
 
 // Stores a dictionary of all text to be displayed
-var dict = {
-    mainTitle = {
-        en = `Welcome to the RL Ireland Wiki!`,
-        ga = `Fáilte go dtí Wiki RL Ireland!`
+const dict = {
+    mainTitle: {
+        en: `Welcome to the RL Ireland Wiki!`,
+        ga: `Fáilte go dtí Wiki RL Ireland!`
     },
-    mainContent = {
-        en = `This is a wiki for all things RLI`,
-        ga = `Is é seo wiki fa choinne RLI`
+    mainContent: {
+        en: `This is a wiki for all things RLI`,
+        ga: `Is é seo wiki fa choinne RLI`
     },
-    langChangeText = {
-        en = `This page is available in <button type="button" onclick="langChange()" class="text-button"><strong>English</strong></button>.`,
-        ga = `Tá an leitheanach seo ar fáil i <button type="button" onclick="langChange()" class="text-button"><strong>nGaeilge</strong></button>.`
+    langChangeText: {
+        en: `This page is available in <button type="button" onclick="langChange()" class="text-button"><strong>English</strong></button>.`,
+        ga: `Tá an leitheanach seo ar fáil i <button type="button" onclick="langChange()" class="text-button"><strong>nGaeilge</strong></button>.`
     }
 }
 
