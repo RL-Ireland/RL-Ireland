2b557a2ab63348d4c6c8fc6027ee38b33fba8c60
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-06-02 07:47:35 +0100

    add dictionary object, attempt to get that working

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ if (!firstTime) {
     localStorage.setItem("lang", 1);
 }
 
+// Stores a dictionary of all text to be displayed
+var dict = {
+    mainTitle = {
+        en = `Welcome to the RL Ireland Wiki!`,
+        ga = `Fáilte go dtí Wiki RL Ireland!`
+    },
+    mainContent = {
+        en = `This is a wiki for all things RLI`,
+        ga = `Is é seo wiki fa choinne RLI`
+    },
+    langChangeText = {
+        en = `This page is available in <button type="button" onclick="langChange()" class="text-button"><strong>English</strong></button>.`,
+        ga = `Tá an leitheanach seo ar fáil i <button type="button" onclick="langChange()" class="text-button"><strong>nGaeilge</strong></button>.`
+    }
+}
+
 // Changes language and stores cookie once changed
 function langChange() {
     if (localStorage.getItem("lang") == 1) {
@@ -19,25 +35,14 @@ function langChange() {
 }
 
 // Loads page with language choices for title
-window.onload = function() {
-    if (localStorage.getItem("lang") == 1) {
-        return document.getElementById("homepageTitle").innerHTML = `Welcome to the RL Ireland Wiki!`;
-    } else if (localStorage.getItem("lang") == 2) {
-        return document.getElementById("homepageTitle").innerHTML = `Fáilte go dtí Wiki RL Ireland!`;
-    } else {
-        return console.log("Error: language not loaded correctly");
-    }
+if (localStorage.getItem("lang") == 1) {
+    document.getElementById("homepageTitle").innerHTML = dict.mainTitle.en;
+    document.getElementById("innerPageContent").innerHTML = dict.mainContent.en;
+    document.getElementById("langChangeText").innerHTML = dict.langChangeText.en;
+} else if (localStorage.getItem("lang") == 2) {
+    document.getElementById("homepageTitle").innerHTML = dict.mainTitle.ga;
+    document.getElementById("innerPageContent").innerHTML = dict.mainContent.ga;
+    document.getElementById("langChangeText").innerHTML = dict.langChangeText.ga;
+} else {
+    console.log("Error: language not loaded correctly");
 }
-
-// Loads page with language choices
-window.onload = function() {
-    if (localStorage.getItem("lang") == 1) {
-        return document.getElementById("innerPageContent").innerHTML = `This is a wiki for all things RLI`;
-    } else if (localStorage.getItem("lang") == 2) {
-        return document.getElementById("innerPageContent").innerHTML = `Fáilte go dtí Wiki RL Ireland!`;
-    } else {
-        return console.log("Error: language not loaded correctly");
-    }
-}
-
-console.log(localStorage.getItem("lang"));
