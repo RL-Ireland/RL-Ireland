f7036108d1980296802f821c5d2e41397ab00994
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-06-03 10:34:40 +0100

    fix links not working

--- main.js
+++ main.js
@@ -22,16 +22,16 @@ const dict = {
         ga: `This page is available in <button type="button" onclick="langChange()" class="text-button"><strong>English</strong></button>.`
     },
     headerBanner: {
-        en: `<li><a href="../index.html">Home</a></li>
-        <li><a href="timeline.html">Timeline</a></li>
-        <li><a href="../Quotes/quotes.html">Quotes</a></li>
-        <li><a href="../Tournaments/tournaments.html">Tournaments</a></li>
-        <li><a href="../Personalities/personalities.html">Personalities</a></li>`,
-        ga: `<li><a href="../index.html">Baile</a></li>
-        <li><a href="timeline.html">Amlíne</a></li>
-        <li><a href="../Quotes/quotes.html">Frásaí</a></li>
-        <li><a href="../Tournaments/tournaments.html">Turnaimint</a></li>
-        <li><a href="../Personalities/personalities.html">Pearsantachtaí</a></li>`
+        en: `<li><a href="index.html">Home</a></li>
+        <li><a href="Timeline/timeline.html">Timeline</a></li>
+        <li><a href="Quotes/quotes.html">Quotes</a></li>
+        <li><a href="Tournaments/tournaments.html">Tournaments</a></li>
+        <li><a href="Personalities/personalities.html">Personalities</a></li>`,
+        ga: `<li><a href="index.html">Baile</a></li>
+        <li><a href="Timeline/timeline.html">Amlíne</a></li>
+        <li><a href="Quotes/quotes.html">Frásaí</a></li>
+        <li><a href="Tournaments/tournaments.html">Turnaimint</a></li>
+        <li><a href="Personalities/personalities.html">Pearsantachtaí</a></li>`
     }
 }
 
